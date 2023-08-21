221214920a644d8d28b644acc5eff898fcfcb015
Brian Lim <brian.q.lim@gmail.com>
2023-08-21 19:47:18 +0900

    Attempt to request games from IGDB example page

---
+++ ROM-Record/src/components/GameList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <h1>Games</h1>
+      <ul>
+        <li v-for="game in games" :key="game.id">{{ game.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        games: []
+      };
+    },
+    mounted() {
+      this.fetchGames();
+    },
+    methods: {
+      async fetchGames() {
+        try {
+          const response = await axios.get(
+            `https://api.igdb.com/v4/games`,
+            {
+              headers: {
+                'Client-ID': keys.env.VUE_APP_IGDB_API_KEY,
+              }
+            }
+          );
+          this.games = response.data;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  };
+  </script>
