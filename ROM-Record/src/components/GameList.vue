bdbaea519cfd9c663d6ea940b9695b41ba536efa
BM85-Hz <53278075+BM85-Hz@users.noreply.github.com>
2023-08-24 10:25:42 +0900

    swap auth with AWS proxy (not working)

--- ROM-Record/src/components/GameList.vue
+++ ROM-Record/src/components/GameList.vue
@@ -18,61 +18,28 @@
   import { useAuthStore } from '../stores/auth.js';
   
   export default {
-    data() {
-      return {
-        games: [],
-        loading: true,
-        error: false
-      };
-    },
-    async mounted() {
-        await this.fetchTwitchAuth();
-        this.fetchGames();
-    },
-    methods: {
         //fetch on initial login
         //run on initial page load, put OAuth in a store
         //gets OAuth token needed for authorization in fetchGames()
-        async fetchTwitchAuth(){
-            try {
-                const response = await axios.get(
-                    'https://id.twitch.tv/oauth2/token?client_id=' + import.meta.env.VUE_APP_CLIENTID
-                    + '&client_secret=' + import.meta.env.VUE_APP_SECRET 
-                    + '&grant_type=client_credentials'
-                );
-            useAuthStore().updateAuthToken(response.data.access_token); 
-            } catch (error) {
-                console.error(error);
-                console.error(error.response);
-                this.error = true;
-            } finally {
-                this.loading = false;
-            }
-        },
-
+        
         //call to IGDB API that gets list of games
-        async fetchGames() {
-            try {
-            const response = await axios.get(
-                `https://api.igdb.com/v4/games`,
-                {
-                headers: {
-                    'Client-ID': import.meta.env.VUE_APP_CLIENTID,
-                    'Authorization': 'Bearer ' + useAuthStore().authenticatedToken,
-                    }
-                }
-            );
-            this.games = response.data;
-            this.loading = false;
-            } catch (error) {
-                console.error(error);
-                console.error(error.response);
-                this.error = true;
-            } finally {
-                this.loading = false;
-            }
-        }
-    }
+        var myHeaders = new Headers();
+        myHeaders.append("x-api-key", "DwqGnrnS3CbBHegC6TzA4sHNlKGnq4w79eD8vW43");
+        myHeaders.append("Content-Type", "text/plain");
+
+        var raw = "fields screenshots.url; limit 10;";
+
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+        };
+
+        fetch("https://lnattp9ct5.execute-api.us-west-2.amazonaws.com/production/v4/games", requestOptions)
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
   };
   </script>
 
