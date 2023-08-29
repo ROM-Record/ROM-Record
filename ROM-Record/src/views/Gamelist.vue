2f005ef8b3b23408b3d2c54a47a28074cca17a0f
BlargBetaa <103034523+BlargBetaa@users.noreply.github.com>
2023-08-29 14:41:33 +0900

    Moved game search function to Gamelist.vue
    To stop the game search function from clipping with everything, I removed it from app.vue to its own view.

---
+++ ROM-Record/src/views/Gamelist.vue
@@ -0,0 +1,53 @@
+<script setup>
+import SearchBar from '../src/components/SearchBar.vue'
+import GameRecord from '../src/components/GameRecords.vue'
+</script>
+
+<template>
+  
+    <header>
+  
+      <div class="wrapper">
+        
+        <nav>
+          <SearchBar/>
+          <GameRecord/>
+        </nav>
+      </div>
+    </header>
+  
+</template>
+
+<style scoped>
+    .wrapper {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+</style>
+
+<style>
+    @font-face {
+        font-family: 'NewRodin';
+        src: url('assets/FOT-NewRodin Pro B.otf') format('opentype');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'DelaGothicOne';
+        src: url('assets/DelaGothicOne-Regular.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    #new-font{
+        font-family: 'NewRodin';
+        font-family: 'DelaGothicOne';
+    }
+</style>
