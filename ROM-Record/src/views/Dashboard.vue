b29a0399cf75c13b1a4806c77025103d412e20a1
Brian Lim <brian.q.lim@gmail.com>
2023-08-31 06:27:07 +0900

    Got rid of empty links

--- ROM-Record/src/views/Dashboard.vue
+++ ROM-Record/src/views/Dashboard.vue
@@ -7,26 +7,10 @@
       <RouterLink to="/Records">Records</RouterLink>
     </div>
     <div class="menu-item">
-      <RouterLink to="/Achievement">Achievements</RouterLink>
-    </div>
-    <div class="menu-item">
-      <RouterLink to="/ViewGames">View Games</RouterLink>
-    </div>
-    <div class="menu-item">
-      <RouterLink to="/EnterGames">Enter Games</RouterLink>
-    </div>
-    <!--<div class="menu-item"> -->
-     <!-- <a href="#">Catalog</a> -->
-    <!--</div> -->
-    <div class="menu-item">
-      <RouterLink to="/Status">Status</RouterLink>
-    </div>
-    <div class="menu-item">
       <RouterLink to="/Logout">Logout</RouterLink>
       <RouterView/>
-      </div>
-     
-</div>
+    </div>
+  </div>
  
 </body>
 </template>
