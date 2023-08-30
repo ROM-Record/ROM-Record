4a70c07e5b10c343b48fdf766fba50764b0dae33
MourningAngel <47402068+faduckie@users.noreply.github.com>
2023-08-30 11:30:39 -0700

    Can add games to backlog from search

--- ROM-Record/src/components/ListLogging.vue
+++ ROM-Record/src/components/ListLogging.vue
@@ -7,7 +7,7 @@
         <ul>
             <li v-for="(game, index) in backlog" :key="index">
                 {{ game.title }}
-               <!--- [Added: {{ formatDate(game.timestamp) }}] -->
+                [Added: {{ formatDate(game.timestamp) }}]
                 <select v-model="game.status" @change="updateStatus(index)">
                     <option value="Playing">Playing</option>
                     <option value="Want to play">Want to play</option>
@@ -23,31 +23,30 @@
 </template>
 
 <script>
+import Stopwatch from '../components/Stopwatch.vue';
 import { doc, setDoc, deleteDoc } from 'firebase/firestore';
 import { db, auth } from '../firebaseResources';
-import Stopwatch from '../components/Stopwatch.vue';
-
 
 export default {
-    props:{
-        gameName: String,
-        igId: String,
-    },
+props:{
+    gameName: String,
+    igId: String,
+},
 components: {
     Stopwatch
 },
-    data() {
-        return {
-            userInput: {
-                title: '',
-                date: null,
-                status: 'Want to play',
-                igdbID: ''
-            },
-            backlog: []
-        };
+data() {
+    return {
+    userInput: {
+        title: '',
+        date: null,
+        status: 'Want to play',
+        igdbID: '',
     },
-    methods: {
+    backlog: []
+    };
+},
+methods: {
         async addGame() {
             if (this.gameName.trim() !== '') {
                 console.log(`Logging game: ${this.gameName}`); 
@@ -65,23 +64,6 @@ components: {
             this.userInput.status = index;
             await setDoc(doc(db, 'users', uid, 'backlog', this.userInput.title), this.userInput);
         },
-        formatDate(date) {
-            return new Intl.DateTimeFormat('en-US', {
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-                hour: 'numeric',
-                minute: 'numeric'
-            }).format(date);
-        },
-        /*async removeGame(index) {
-            
-        }*/
-    },
-    updateStatus(index) {
-    // Firebase update code goes here
-    this.backlog[index].status = this.backlog[index].status;
-    },
     formatDate(date) {
       return new Intl.DateTimeFormat('en-US', {
         year: 'numeric',
@@ -98,6 +80,7 @@ components: {
         this.backlog.push({ title: timeRecorded, timestamp: new Date() });
         console.log(`Logging time: ${timeRecorded}`);
     },
+}
 };
 </script>
 
